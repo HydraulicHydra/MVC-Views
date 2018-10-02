9bb047c84ab1ad2512d2ea65eb3b115bf120355b
Garrett Surber <38016069+HydraulicHydra@users.noreply.github.com>
2018-10-02 00:55:13 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mvc3ViewDemo.Models;
+
+namespace Mvc3ViewDemo.Controllers
+{
+    public class Blog
+    {
+        public string Name;
+        public string URL;
+    }
+
+    public class HomeController : Controller
+    {
+
+        List<Blog> topBlogs = new List<Blog>
+      {
+          new Blog { Name = "ScottGu", URL = "http://weblogs.asp.net/scottgu/"},
+          new Blog { Name = "Scott Hanselman", URL = "http://www.hanselman.com/blog/"},
+          new Blog { Name = "Jon Galloway", URL = "http://www.asp.net/mvc"}
+      };
+
+        public ActionResult IndexNotStonglyTyped()
+        {
+            return View(topBlogs);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            return View();
+        }
+
+        public ActionResult StonglyTypedIndex()
+        {
+            return View(topBlogs);
+        }
+    }
+}
